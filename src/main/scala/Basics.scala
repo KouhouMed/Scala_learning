fb7723414b98523213a8571b029fb54f7ba80c8e
KouhouMed <mohamedkouhou@gmail.com>
2024-07-22 00:08:15 +0200

    basics

---
+++ src/main/scala/Basics.scala
@@ -0,0 +1,11 @@
+object Basics extends App{
+  // defining a value
+  val meaningOfLife: Int = 42 // const int meaningOfLife = 42;
+
+  // Int, Boolean, Char, Double, Float, String
+  val aBoolean = false // type is optional
+
+  // strings and string operations
+  val aString = "I love Scala !"
+
+}

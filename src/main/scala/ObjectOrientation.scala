17b97488175308d3aa13a15d9820ac1136434ce2
KouhouMed <mohamedkouhou@gmail.com>
2024-07-22 23:27:11 +0200

    object orientation

---
+++ src/main/scala/ObjectOrientation.scala
@@ -0,0 +1,17 @@
+object ObjectOrientation extends App {
+  // java equivalent: public static void main(String[] args)
+
+  // class and instance
+  class Animal {
+    // define fields
+    val age: Int = 0
+
+    // define methods
+    def eat(): Unit = println("I'm eating")
+  }
+
+  val anAnimal = new Animal
+
+  // inheritance
+
+}
